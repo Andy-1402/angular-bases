3d71219172dc80f773dece08a066b5c981ffab68
AndyB2 <andychafla14@gmail.com>
2024-12-24 20:19:16 -0500

    Fin de seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HeroesModule } from './heroes/heroes.module';
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeroesModule,
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
